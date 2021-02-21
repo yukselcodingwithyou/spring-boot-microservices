0626b8d82fc193d06d9f636fa5419b2a0a1d853a
yukselcoding@gmail.com <yuksel.ozdemir@tokeninc.com>
2021-02-21 12:03:15 +0300

    03. init product-customer-service

---
+++ product-customer-service/src/test/java/com/yukselcoding/productcustomerservice/ProductCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukselcoding.productcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
