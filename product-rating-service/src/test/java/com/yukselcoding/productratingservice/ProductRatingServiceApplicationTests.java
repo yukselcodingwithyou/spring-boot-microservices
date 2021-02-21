faca50e4879725c0d463415753c8e08b8e6477dc
yukselcoding@gmail.com <yuksel.ozdemir@tokeninc.com>
2021-02-21 12:00:01 +0300

    02. init product-rating-service

---
+++ product-rating-service/src/test/java/com/yukselcoding/productratingservice/ProductRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukselcoding.productratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
