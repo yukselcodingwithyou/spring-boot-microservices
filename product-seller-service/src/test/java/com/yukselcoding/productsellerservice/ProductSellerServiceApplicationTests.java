eb71bc88f4f2bb81f3ac644cc02d9f18373af88a
yukselcoding@gmail.com <yuksel.ozdemir@tokeninc.com>
2021-02-21 12:07:41 +0300

    04. init product-seller-service

---
+++ product-seller-service/src/test/java/com/yukselcoding/productsellerservice/ProductSellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukselcoding.productsellerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSellerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
