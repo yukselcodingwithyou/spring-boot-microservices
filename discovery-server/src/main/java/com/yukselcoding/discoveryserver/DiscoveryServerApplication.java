cb825db3cfe4edbbe4b064626575a02af97b71c0
yukselcoding@gmail.com <yuksel.ozdemir@tokeninc.com>
2021-02-28 09:24:41 +0300

    08. up & running discovery-server

--- discovery-server/src/main/java/com/yukselcoding/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/yukselcoding/discoveryserver/DiscoveryServerApplication.java
@@ -2,7 +2,10 @@ package com.yukselcoding.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
